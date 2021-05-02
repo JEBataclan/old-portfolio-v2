c659cb5f8e4339842209a0ce1e5e809978020c26
Jan Erickson Bataclan <jebataclan03@gmail.com>
2021-05-03 02:59:18 +0800

    Add Header and Navbar with mvery minor contents.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi!</h1>
+    <div>
+      <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <>
+      <p>
+        Hi, I'm Jan Erickson S. Bataclan! I'm a programmer that also knows a
+        li'l bit of 3D rendering.
+      </p>
+      <h1>
+        JE
+        <br />
+        Bb
+      </h1>
+    </>
+  );
+}
